15b2f3c290820289d291a3a8224bbd05fd8b445f
Navid Dezashibi <dezashibi@gmail.com>
2024-08-14 13:19:01 +0400

    update example starting comment block

--- examples/semaphore.c
+++ examples/semaphore.c
@@ -1,3 +1,18 @@
+// ***************************************************************************************
+//    Project: dthreads -> https://github.com/dezashibi-c/dthreads
+//    File: semaphore.c
+//    Date: 2024-08-14
+//    Author: Navid Dezashibi
+//    Contact: navid@dezashibi.com
+//    Website: https://www.dezashibi.com | https://github.com/dezashibi
+//    License:
+//     Please refer to the LICENSE file, repository or website for more information about
+//     the licensing of this work. If you have any questions or concerns,
+//     please feel free to contact me at the email address provided above.
+// ***************************************************************************************
+// *  Description: Refer to readme for documentation or dthreads.h
+// ***************************************************************************************
+
 #define DTHREAD_IMPL
 #include "../dthreads/dthread.h"
 
