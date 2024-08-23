43df3d3c14476fb09f5b8e049d48e5ebbd32cb64
Navid Dezashibi <dezashibi@gmail.com>
2024-08-23 15:14:31 +0400

    Update rwlock.c

--- examples/rwlock.c
+++ examples/rwlock.c
@@ -1,3 +1,18 @@
+// ***************************************************************************************
+//    Project: dthreads -> https://github.com/dezashibi-c/dthreads
+//    File: rwlock.c
+//    Date: 2024-08-23
+//    Author: Navid Dezashibi
+//    Contact: navid@dezashibi.com
+//    Website: https://www.dezashibi.com | https://github.com/dezashibi
+//    License:
+//     Please refer to the LICENSE file, repository or website for more information about
+//     the licensing of this work. If you have any questions or concerns,
+//     please feel free to contact me at the email address provided above.
+// ***************************************************************************************
+// *  Description: Refer to readme for documentation or dthread.h
+// ***************************************************************************************
+
 #define DTHREAD_DEBUG
 #define DTHREAD_IMPL
 #include "../dthreads/dthread.h"
